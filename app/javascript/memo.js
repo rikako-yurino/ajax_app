b12241f4234c7155ea0bbb8b94790d5833479074
rikako-yurino <dawnpurple.1989@gmail.com>
2021-03-01 11:42:16 +0900

    コントローラーにリクエストを送信する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,5 +1,14 @@
 function post (){
-  console.log("イベント発火");
+  const submit = document.getElementById("submit");
+  submit.addEventListener('click', (e) =>{
+    e,preventDefault();
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XMR = new XMLHttpRequest();
+    XMR.open("post", "/posts", true);
+    XMR.responseType = "json";
+    XMR.send(formData);
+  });
 }
 
 window.addEventListener('load', post);
